40b05071fcd4f6c86db221576eed4e5d66a952dc
Parth Gosai <parthgosai605@gmail.com>
2025-01-16 15:27:56 +0530

    My first web app

---
+++ Node JS/Index.js
@@ -0,0 +1,11 @@
+var a= 25;
+var a= 30;
+var a= 40;
+// var sum= a+b+c
+// console.log(a,b)
+// console.log(sum)
+// for (i=50;i>=1;i--){
+//     if(i%2==0) console.log(i,"is even")
+//         else console.log(i,"is Odd")
+// }
+
